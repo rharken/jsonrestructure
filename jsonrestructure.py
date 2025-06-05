8404b4e87ac8defb6ce6c80fc85a44269981cfe8
Richard Harken <rhark3n@gmail.com>
2025-06-05 09:23:18 -0400

    Additional restructure experimentation

--- jsonrestructure.py
+++ jsonrestructure.py
@@ -2,33 +2,23 @@
 """
 import json
 import time
-from weatherjson import WeatherJSON
-from flightjson import FlightJSON
 
 def main():
     """ Main function for program
     """
-    wfile = list[WeatherJSON]
-    startweather = time.time()
-    with open("weather.json", encoding="utf-8") as f:
-        wfile = [WeatherJSON(**json.loads(row)) for row in f]
+    startrestructure = time.time()
+    with open("gamedata.json", encoding="UTF8") as f:
+        for row in f:
+            rowjson = json.loads(row)
+            print(rowjson)
 
-    for we in wfile:
-        print(f"High Temp: {we.MaxTemp}")
-    stopweather = time.time()
-    print(f"Weather processing including printing: {stopweather-startweather:.4f}s")
+            for player in rowjson['PerPlayer']['player']:
+                print(f"username: {player['username']}")
+                for characteristic in player['characteristics']:
+                    print(f"\t{characteristic}: {player['characteristics'][characteristic]}")
 
-    startflights = time.time()
-    ffile = list[FlightJSON]
-    with open("flights-1m.json", encoding="utf-8") as f:
-        ffile = [FlightJSON(**json.loads(row)) for row in f]
-
-    print(f"Flight data for row 1234: {ffile[1234-1]}")
-    print(
-        f"Formatted date for the same record: {ffile[1234-1].FL_DATE.strftime('%m/%d/%Y')}")
-    stopflights = time.time()
-
-    print(f"Fights processing including printing one row: {stopflights-startflights:.4f}s")
+    stoprestructure = time.time()
+    print(f"Processing time: {stoprestructure-startrestructure:.4f}s")
 
 
 if __name__ == "__main__":
